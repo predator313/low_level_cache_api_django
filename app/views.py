20940a0fad6e26f77331f147a3e71ff03a850b89
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-01 16:26:08 +0530

    making views function for checking if key is present or not

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.cache import cache
 
 # Create your views here.
+def fun(request):
+    mv=cache.get('movie','not_available')
+    if mv=='not_available':
+        cache.set('movie','pathan',30)
+    mv=cache.get('movie')
+    return render(request,'app/home.html',{'mv':mv})
