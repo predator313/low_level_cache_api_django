5e4032102b90a5fc76b8a619210f3e7f4f67aae6
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-01 20:23:01 +0530

    all done

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from django.core.cache import cache
 
 # Create your views here.
 def fun(request):
-    mv=cache.get('movie','not_available')
-    if mv=='not_available':
-        cache.set('movie','pathan',30)
-    mv=cache.get('movie')
+    mv=cache.get_or_set('fees',200,30,version=2)
     return render(request,'app/home.html',{'mv':mv})
+    # mv=cache.get('movie','not_available')
+    # if mv=='not_available':
+    #     cache.set('movie','pathan',30)
+    # mv=cache.get('movie')
+    # return render(request,'app/home.html',{'mv':mv})
